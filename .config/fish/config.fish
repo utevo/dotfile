29b3e7dd7f2513b9c44fda0c6797ae151f56f416
Michał Kowieski <michael.kowieski@gmail.com>
2020-05-27 13:53:30 +0200

    Update PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,10 @@ alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 # statship
 starship init fish | source
 
-#
+# Update PATH
 set -gx PATH $HOME/.local/bin $PATH
+
+set -gx PATH $HOME/go/bin $PATH
+
+set -gx PATH $HOME/.cargo/bin $PATH
+
