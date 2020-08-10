315ecfefdf84198ba2b8ec614d15bba6927d9019
utevo <utevo@pop-os.localdomain>
2020-08-10 21:11:58 +0200

    deno

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,10 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
 # statship
 starship init fish | source
 
+# Deno
+set -gx  DENO_INSTALL $HOME/.deno
+set -gx PATH $DENO_INSTALL/bin $PATH
+
 # Update PATH
 set -gx PATH $HOME/.local/bin $PATH
 set -gx PATH /usr/local/go/bin $PATH
@@ -12,6 +16,7 @@ set -gx PATH $HOME/.cargo/bin $PATH
 set -gx PATH $HOME/.yarn/bin $PATH
 set -gx PATH $HOME/bin $PATH
 set -gx PATH /snap/bin $PATH
+set -gx PATH 
 
 # fnm
 set PATH $HOME/.fnm $PATH
@@ -24,3 +29,4 @@ status --is-interactive; and source (pyenv init -|psub)
 # UID and GID
 set -gx UID (id -u)
 set -gx GID (id -g)
+
